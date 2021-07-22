944ba151dab79185dd7a05d78588e5a34c289cc6
timo-berg <timotheus.berg@pm.me>
2021-07-22 17:30:59 +0200

    Added documentation and refactored into functions

--- eegplot.jl
+++ eegplot.jl
@@ -8,31 +8,53 @@ include("load_eeglab.jl")
 data,srate,evts_df,chanlocs_df,EEG = import_eeglab("sub35_preprocessed.set")
 
 
+
 ##
 
 nchan = size(data,1)
 nsamples = size(data,2)
 
+real_data = deepcopy(data)
+test_data = zeros((nchan,nsamples))
+for i in StepRange(1, Int(srate/10) , nsamples)
+    test_data[:,i] = ones((1,nchan))
+end
+data = test_data
+
 chanlabels = chanlocs_df[:, :labels]
 
 # Parameters to set
 scale = 1
-offset = 100
-nsample_to_show = 1000
+offset = 5
+nsample_to_show = 1000 # only for initial plotting
 
+tag_height = (nchan+5)*offset*scale
+plot_high = (nchan+10)*offset*scale
+plot_low = -offset*scale
+event_upper_lim = tag_height/plot_high-0.01
 
 ##
 
-function update_time_ticks(axis, value)
-    # xtick_pos = 1:100:nsample_to_show_obs[]
-    xtick_pos = LinRange(Int(nsample_to_show_obs[]*0.1),Int(nsample_to_show_obs[]*0.9), 10)
-    xtick_pos_label = map(x->round((value + x)/srate, digits=2), xtick_pos)
+"""
+Create 10 x-ticks from 10% to 90% of the x-axis as time in seconds.
+time_idx should be the lower bound of the plotted data.
+"""
+function set_time_ticks(axis, time_idx)
+    plot_range = upper_bound[] - lower_bound[]
+    xtick_pos = LinRange(Int(plot_range*0.1),Int(plot_range*0.9), 10)
+    # Convert x_pos to time
+    xtick_pos_label = map(x->round((time_idx + x)/srate, digits=2), xtick_pos)
     axis.xticks = (xtick_pos, ["$(x)s" for x in xtick_pos_label])
 end
 
+"""
+Check what events lines are in the current plot range and make them visible.
+Make events outside of this range invisible.
+"""
 function redraw_event_markers(lines)
-    # Draw new events and store them in event array
+    # Check for visibility
     vis_events = lower_bound[] .< evts_df[:,:latency] .< upper_bound[]
+    
     for (event_idx, event) in enumerate(lines[vis_events])
         event.visible = true
         tags[event_idx].visible = true
@@ -43,27 +65,40 @@ function redraw_event_markers(lines)
     end
 end
 
+"""
+Check what events tags are in the current plot range and make them visible.
+Make events outside of this range invisible.
+"""
 function redraw_event_text(tags)
+    # Check for visibility
     vis_events = lower_bound[] .< evts_df[:,:latency] .< upper_bound[]
-    for (tag_idx, tag) in enumerate(tags[vis_events])
-        # index of tag in unfiltered array
+    for tag in tags[vis_events]
+        # Index of tag in unfiltered array
         idx_unfiltered = findfirst((x)-> x==tag, event_tags)
+        # Observable position doesn't work for text -> manually update position
         corrected_lat = evts_df[idx_unfiltered,:latency] - lower_bound[]
         tag.attributes.attributes[:position][] = (corrected_lat, tag_height)
         tag.visible = true
     end
-    for (tag_idx, tag) in enumerate(tags[.!vis_events])
+    for tag in tags[.!vis_events]
         tag.visible = false
     end
 end
 
+"""
+Check what reject regions are in the current plot range and make them visible.
+Make reject regions outside of this range invisible.
+"""
 function redraw_reject_regions(limits, plots)
+    # Create dataframe with lower and upper bounds of the regions
     limits_df = DataFrame( lower=[], upper=[])
     for region in limits
         push!(limits_df, region)
     end
+    # Check what regions are visible
     vis_regions = ((lower_bound[] .< limits_df[:,:lower] .< upper_bound[])
                 .| (lower_bound[] .< limits_df[:,:upper] .< upper_bound[]))
+
     for region in plots[vis_regions]
         region.visible = true
     end
@@ -72,18 +107,25 @@ function redraw_reject_regions(limits, plots)
     end
 end
 
+"""
+Draw a transparent rectangle that represents a region to be rejected
+"""
 function draw_reject_region(rect)
     poly!(axis, rect, raw = true, visible = true, color = RGBAf0(1.0, 0.08, 0.58, 0.3), strokewidth = 0)
 end
 
+"""
+Check if mouse is inside scene.
+"""
 function is_mouseinside(scene)
     return Vec(scene.events.mouseposition[]) in pixelarea(scene)[]
-    # Check that mouse is not inside any other screen
-    # for child in scene.children
-    #     is_mouseinside(child) && return false
-    # end
 end
 
+"""
+Take a rectangle with origin and widths and return the same rectangle but with
+the origin in the lower left corner.
+This ensures that widths are positive.
+"""
 function absrect(rectangle)
     origin = rectangle.origin
     widths = rectangle.widths
@@ -93,6 +135,10 @@ function absrect(rectangle)
     new_rectangle = FRect(new_origin[1], new_origin[2], new_widths[1], new_widths[2])
 end
 
+"""
+Select a time region in the plot when Left-Shift is hold.
+Return the selected region.
+"""
 function select_rectangle(scene; blocking = false, priority = 2, strokewidth = 3.0, kwargs...)
     key = Keyboard.left_shift
     waspressed = Node(false)
@@ -105,7 +151,9 @@ function select_rectangle(scene; blocking = false, priority = 2, strokewidth = 3
         scene, rect, raw = true, visible = false, color = RGBAf0(1.0, 0.08, 0.58, 0.3), strokewidth = 0, transparency = false, kwargs...,
     )
 
+    # Control key press and hold
     on(events(scene).keyboardbutton , priority=priority) do event
+        # Start tracking on key press
         if event.key == key
             if event.action == Keyboard.press && is_mouseinside(scene)
                 mp = mouseposition(scene)
@@ -115,6 +163,7 @@ function select_rectangle(scene; blocking = false, priority = 2, strokewidth = 3
                 return Consume(blocking)
             end
         end
+        # End if key is released
         if !(event.key == key && event.action == Keyboard.repeat)
             if waspressed[] # User has selected the rectangle
                 waspressed[] = false
@@ -131,6 +180,7 @@ function select_rectangle(scene; blocking = false, priority = 2, strokewidth = 3
 
         return Consume(false)
     end
+    # Adapt rectangle depending on mouse position; only if currently tracking.
     on(events(scene).mouseposition, priority=priority) do event
         if waspressed[]
             mp = mouseposition(scene)
@@ -144,108 +194,40 @@ function select_rectangle(scene; blocking = false, priority = 2, strokewidth = 3
     return rect_ret
 end
 
-##
-
-# Set up figure
-fig = Figure(resolution = (2560, 1440))
-axis = fig[1, 1] = Axis(fig, title = "EEG")
-
-# native zoom
-axis.yzoomlock = true
-axis.xzoomlock = true
-axis.xpanlock = true
-axis.ypanlock = true
-axis.xrectzoom = false
-axis.yrectzoom = false
-
-tag_height = (nchan+5)*offset*scale
-plot_high = (nchan+10)*offset*scale
-plot_low = -offset*scale
-event_upper_lim = tag_height/plot_high-0.01
-
-# Set up slider
-slider = Slider(fig[2, 1], range = 1:1:nsamples-nsample_to_show, startvalue = 1)
-slider_time = slider.value
-
-# Time range to plot
-nsample_to_show_obs = Node(nsample_to_show)
-lower_bound = @lift(max(1, Int($slider_time)-Int($nsample_to_show_obs/2)))
-upper_bound = @lift(min(nsamples, $lower_bound+Int($nsample_to_show_obs)))
-range = @lift([$lower_bound,$upper_bound])
-
-# Plot EEG
-channel_plots = []
-for chan in 1:nchan
-    data_chunk = @lift(data[chan,$range[1]:$range[2]])
-    chunk_mean = @lift(mean($data_chunk))
-    data_chunk_plot = @lift($data_chunk .+ (-$chunk_mean + chan*offset) .* scale)
-    append!(channel_plots, [lines!(axis, data_chunk_plot, color = :grey0)])
+"""
+Disable Makie-native scroll zoom, pan scroll and rectangle zoom.
+"""
+function disable_native_zoom(axis)
+    axis.yzoomlock = true
+    axis.xzoomlock = true
+    axis.xpanlock = true
+    axis.ypanlock = true
+    axis.xrectzoom = false
+    axis.yrectzoom = false
 end
 
-# Plot events
-event_plots = map(eachrow(evts_df)) do event
-    plot_pos = @lift(event.latency-$lower_bound)
-    vlines!(axis, plot_pos, ymax=event_upper_lim, visible = false)
-end
-event_tags = map(eachrow(evts_df)) do event
-    plot_pos = @lift(event.latency-$lower_bound)
-    text!(axis.scene, event.type, position=(plot_pos,tag_height), align=(:center, :center), visible=false)
-end
-on(range) do value
-    redraw_events(event_plots)
-    redraw_event_text(event_tags)
-    redraw_reject_regions(reject_limits, reject_plots)
-end
-
-# Set ticks
-axis.yticks = (offset:offset:(nchan)*offset, chanlabels)
-update_time_ticks(axis, 0)
-# dynamic time tick update
-on(slider.value) do value
-    update_time_ticks(axis, value)
-end
-
-# Scrolling zoom
-on(events(fig).scroll, priority = 100) do scroll
-    nsample_to_show_obs[] = clamp(nsample_to_show_obs[]+scroll[2]*30, 150, nsamples)
-    xlims!(0,nsample_to_show_obs[])
-    update_time_ticks(axis, slider.value[])
-end
-
-# Color channel on click
-on(events(fig).mousebutton) do event
-    if event.action == Mouse.press && event.button == Mouse.left
-        mouse_pos = events(fig).mouseposition[]
-        mark_plot = Makie.pick(axis.scene, mouse_pos, 500)[1]
-        if mark_plot in channel_plots
-            if mark_plot.attributes.color[] == :red
-                mark_plot.attributes.color = :grey0
-            else
-                mark_plot.attributes.color = :red
-            end
+"""
+Find channel that is closest to mouseposition at click and color it red.
+If channel is marked already, unmark it.
+"""
+function mark_channel(axis, mouse_pos)
+    mark_plot = Makie.pick(axis.scene, mouse_pos, 500)[1]
+    # Lineplot is an actual EEG plot
+    if mark_plot in channel_plots
+        # If marked, unmark it
+        if mark_plot.attributes.color[] == :red
+            mark_plot.attributes.color = :grey0
+        else
+            mark_plot.attributes.color = :red
         end
     end
-    return Consume(false)
 end
 
-# Keyboard scroll 
-on(events(fig).keyboardbutton) do event
-    if event.action in (Keyboard.press, Keyboard.repeat)
-        if event.key == Keyboard.left
-            slider_time[] = max(1, slider_time[]-100)
-        end
-        if event.key == Keyboard.right
-            slider_time[] = min(slider_time[]+100, nsamples-nsample_to_show)
-        end
-    end
-    return Consume(false)
-end
-
-# Mark time windows
-rect = select_rectangle(axis.scene)
-reject_limits = Array{Float32}[]
-reject_plots = []
-on(rect) do new_rect
+"""
+Create a new reject region at the passed region if it does not overlap with an exisiting one.
+Stores the plot in the reject_plots array and its limits in reject_limits array.
+"""
+function create_new_reject_region(new_rect)
     start_idx = deepcopy(new_rect.origin[1]+max(1, slider_time[]-nsample_to_show_obs[]/2))
     end_idx = deepcopy(start_idx + new_rect.widths[1])
     overlaps = false
@@ -266,33 +248,131 @@ on(rect) do new_rect
     end
 end
 
+"""
+Check if reject region is under passed x-position.
+Delete region from axis, plot_array, and region limits.
+"""
+function delete_reject_region(position)
+    for (region_idx, limit) in enumerate(reject_limits)
+        if limit[1] < position+max(1, slider_time[]-nsample_to_show_obs[]/2) < limit[2]
+            deleteat!(reject_limits, region_idx)
+            delete!(axis, reject_plots[region_idx])
+            deleteat!(reject_plots, region_idx)
+        end
+    end
+end
+##
+
+# Set up figure
+fig = Figure(resolution = (2560, 1440))
+axis = fig[1, 1] = Axis(fig, title = "EEG")
+
+# disable native zoom
+disable_native_zoom(axis)
+
+# Set up time slider
+slider = Slider(fig[2, 1], range = 1:1:nsamples-nsample_to_show, startvalue = 1)
+slider_time = slider.value
+
+# Time range to plot
+nsample_to_show_obs = Node(nsample_to_show)
+lower_bound = @lift(max(1, Int($slider_time)-Int($nsample_to_show_obs/2)))
+upper_bound = @lift(min(nsamples, $lower_bound+Int($nsample_to_show_obs)))
+range = @lift([$lower_bound,$upper_bound])
+
+# Plot EEG
+channel_plots = []
+for chan in 1:nchan
+    # Get data chunk currently to be plottet
+    data_chunk = @lift(data[chan,$range[1]:$range[2]])
+    chunk_mean = @lift(mean($data_chunk))
+    # Mean center, scale and shift to plot all channels above each other in y-direction
+    data_chunk_plot = @lift((($data_chunk .-$chunk_mean)  .* scale) .+ chan*offset)
+    append!(channel_plots, [lines!(axis, data_chunk_plot, color = :grey0)])
+end
+
+# Plot events as vertical lines
+event_plots = map(eachrow(evts_df)) do event
+    plot_pos = @lift(event.latency-$lower_bound)
+    vlines!(axis, plot_pos, ymax=event_upper_lim, visible = false, color = :steelblue1)
+end
+# Plot event tag above event line
+event_tags = map(eachrow(evts_df)) do event
+    plot_pos = @lift(event.latency-$lower_bound)
+    text!(axis.scene, event.type, position=(plot_pos,tag_height), align=(:center, :center), visible=false)
+end
+
+# Plot range changes (i.e. zoom or scroll)
+on(range) do value
+    redraw_events(event_plots)
+    redraw_event_text(event_tags)
+    redraw_reject_regions(reject_limits, reject_plots)
+    set_time_ticks(axis, lower_bound[])
+end
+
+# Set initial ticks
+axis.yticks = (offset:offset:(nchan)*offset, chanlabels)
+axis.yticklabelsize = 10.0
+set_time_ticks(axis, 0)
+
+# Scrolling zoom
+on(events(fig).scroll, priority = 100) do scroll
+    nsample_to_show_obs[] = clamp(nsample_to_show_obs[]-scroll[2]*30, 150, nsamples)
+    xlims!(0,nsample_to_show_obs[])
+end
+
+# Color channel on click
+on(events(fig).mousebutton) do event
+    if event.action == Mouse.press && event.button == Mouse.left
+        mouse_pos = events(fig).mouseposition[]
+        mark_channel(axis, mouse_pos)
+    end
+    return Consume(false)
+end
+
+# Time-scroll on left/right arrow key
+on(events(fig).keyboardbutton) do event
+    if event.action in (Keyboard.press, Keyboard.repeat)
+        if event.key == Keyboard.left
+            slider_time[] = max(1, slider_time[]-100)
+        end
+        if event.key == Keyboard.right
+            slider_time[] = min(slider_time[]+100, nsamples-nsample_to_show)
+        end
+    end
+    return Consume(false)
+end
+
+# Mark time windows
+rect = select_rectangle(axis.scene)
+reject_limits = Array{Float32}[]
+reject_plots = []
+on(rect) do new_rect
+    create_new_reject_region(new_rect)
+end
+
 # Delete time window on right click
 on(events(fig).mousebutton) do event
     if event.button == Mouse.right && event.action == Mouse.press
         mp_x = mouseposition(axis.scene)[1]
-        for (region_idx, limit) in enumerate(reject_limits)
-            if limit[1] < mp_x+max(1, slider_time[]-nsample_to_show_obs[]/2) < limit[2]
-                deleteat!(reject_limits, region_idx)
-                delete!(axis, reject_plots[region_idx])
-                deleteat!(reject_plots, region_idx)
-            end
-        end
+        delete_reject_region(mp_x)
     end
 end
 
-# No left/right margin
-tightlimits!(axis, Left(), Right())
+# Set visible limits (doesn't affect plotted data)
+tightlimits!(axis, Left(), Right()) # No left/right margin
 ylims!(plot_low, plot_high)
 
-set_window_config!(focus_on_show = true)
 fig
 ##
 
 # TODO
-# - add event tags http://makie.juliaplots.org/stable/plotting_functions/text.html#text
-#   set new text position: event_tags[3].attributes.attributes[:position][] = (266.0, 13400)
-#   use lower_bound[] .< evts_df[:,:latency]-lower_bound[] .< upper_bound[] for text
+# - menu
+#   - load data
+# - scale on ctrl-hold
+# - plot title is filename
 #
 # OPTIONAL
 # - add channel scroll
+# - show mini plot under time-scroll that visualizes position and zoom level
 
